2e5e9b8564d6f1daaef0a01f82e465ccafbb633b
Sonya Tkachenko <94766215+ssonyau@users.noreply.github.com>
2023-04-20 17:52:58 +0300

    .

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import Logo from './logo.svg'
+import './App.css'
+import { Button} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+    return (
+        <div>
+            <h1> Hello world </h1>
+            <Button variant="primary"> Button </Button>
+        </div>
+    );
+}
+
+export default App;
